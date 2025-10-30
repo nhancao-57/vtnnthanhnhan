1d889a146c2381ed6115c689d5edf6f420d0e289
nhancao-57 <nhan.caovt507@gmail.com>
2025-10-30 19:34:59 +1100

    Update

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,14 @@
+const CACHE_NAME = 'myapp-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-      ]);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
